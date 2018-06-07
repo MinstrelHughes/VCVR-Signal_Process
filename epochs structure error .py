bf0d156b0b8c1c04a8c3adb5a89556056f56ff67
vegebun <39867607+vegebun@users.noreply.github.com>
2018-06-07 17:30:10 -0500

    Update epochs structure error .py

--- epochs structure error .py
+++ epochs structure error .py
@@ -39,14 +39,39 @@ print('Unique event codes:', np.unique(events[:, 2]))
 # to save time and memory we'll just look at the auditory conditions
 # for now.
 
-
-
+event_id = {'Auditory/Left': 897, 'Auditory/Right': 898}
 #ValueError: No matching events found for Auditory/Left (event id 1)
-
+print('我是分割线----------')
 epochs = mne.Epochs(raw, events, event_id, tmin=-0.1, tmax=1,
                     baseline=(None, 0), preload=True)
 print(epochs)
 
-epochs = mne.Epochs(raw, events, event_id, tmin=-0.1, tmax=1,
-                    baseline=(None, 0), preload=True)
-print(epochs)
+#epochs = mne.Epochs(raw, events, 898, tmin=-0.1, tmax=1,
+                   # baseline=(None, 0), preload=True)
+#print(epochs)
+
+print(epochs.events[:3])
+print(epochs.event_id)
+print('我是分割线----------')
+
+print(epochs[1:5])
+print(epochs['Auditory/Right'])
+
+# These will be epochs objects
+for i in range(3):
+    print(epochs[i])
+
+# These will be arrays
+for ep in epochs[:2]:
+    print(ep)
+    
+print('-----------我是分割线----------')
+
+ev_left = epochs['Auditory/Left'].average()
+ev_right = epochs['Auditory/Right'].average()
+
+f, axs = plt.subplots(1 2,figsize=(10, 5))
+_ = f.suptitle('Left / Right auditory', fontsize=20)
+_ = ev_left.plot(axes=axs[:, 0], show=False, time_unit='s')
+_ = ev_right.plot(axes=axs[:, 1], show=False, time_unit='s')
+plt.tight_layout()
